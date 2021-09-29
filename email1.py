aea8704e0c518250a0a7795629a33b2a99ea83a9
rishabh2023 <rishabh2023@gmail.com>
2021-09-29 13:42:54 +0530

    first commit

--- email1.py
+++ email1.py
@@ -8,6 +8,6 @@ server.login(SenderAddress,passwrd1)
 msg = 'Hello Dear,Congratulations Your CICD model run successfully from \n Our Best wishes with you. \n Email1\nThankyou' 
 subject = 'Congratulations Your account Successfully Activated'
 body = 'subject:{}\n\n{}'.format(subject,msg)
-server.sendmail(SenderAddress,'sahuji2607@gmail.com',body)
+server.sendmail(SenderAddress,'softwebuc@gmail.com',body)
 server.quit() 
 print('Email sent...')
