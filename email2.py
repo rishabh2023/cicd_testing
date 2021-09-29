31eebcc78563a9fea57aac83661b96bac53008c6
rishabh2023 <rishabh2023@gmail.com>
2021-09-29 13:37:28 +0530

    first commit

---
+++ email2.py
@@ -0,0 +1,13 @@
+import smtplib
+SenderAddress = "bestlib01@gmail.com" 
+# Here Enter Senders Email address
+passwrd1  = 'buyxjsuqainevdpl'
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(SenderAddress,passwrd1)
+msg = 'Hello Dear,Congratulations Your CICD model run successfully from \n Our Best wishes with you. \n Email2\nThankyou' 
+subject = 'Congratulations Your account Successfully Activated'
+body = 'subject:{}\n\n{}'.format(subject,msg)
+server.sendmail(SenderAddress,'sahuji2607@gmail.com',body)
+server.quit() 
+print('Email sent...')
